30906fb21e170257b9312368b22c684042a26b80
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2025-04-01 18:44:56 +0530

    hero section, service section , pricing section,feature section and year section and some improvements are done

---
+++ HouseFix/assests/script/script.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const toggler = document.querySelector(".navbar-toggler");
+//   const collapse = document.querySelector(".navbar-collapse");
+//   const closeBtn = document.querySelector(".close-btn");
+
+//   function closeMenu() {
+//       collapse.classList.remove("show");
+
+//   }
+
+//   toggler.addEventListener("click", function () {
+//       collapse.classList.toggle("show");
+//   });
+
+//   closeBtn.addEventListener("click", closeMenu);
+
+// });
