4a2f71e12b45538fc6815236757760ab9afc9244
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2025-04-01 10:05:12 +0530

    fixes

--- HouseFix/script.js
+++ HouseFix/script.js
@@ -1,24 +1,17 @@
-// const toggleOpen = document.querySelector(".navbar-collapse")
-// const toggleClose = document.querySelector(".toggle-close")
-
-// // console.log(navlinks)
-
-// toggleOpen.addEventListener("click", open)
-// toggleClose.addEventListener("click", close)
-
-// function open() {
-//     navlinks.style.transform = "translateX(0px)"
-// }
-
-// function close() {
-//     navlinks.style.transform = "translateX(1000px)"
-// }
-
 document.addEventListener("DOMContentLoaded", function () {
-    const toggler = document.querySelector(".navbar-toggler");
-    const menu = document.querySelector(".navbar-collapse");
-  
-    toggler.addEventListener("click", function () {
-      menu.classList.toggle("show");
-    });
+  const toggler = document.querySelector(".navbar-toggler");
+  const collapse = document.querySelector(".navbar-collapse");
+  const closeBtn = document.querySelector(".close-btn");
+
+  function closeMenu() {
+      collapse.classList.remove("show");
+
+  }
+
+  toggler.addEventListener("click", function () {
+      collapse.classList.toggle("show");
+  });
+
+  closeBtn.addEventListener("click", closeMenu);
+
 });
