aebb15e0bdec662336c393b5c00b3ce180c20ea9
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2025-04-02 18:43:21 +0530

    customer card section,blog section and footer pushed.

--- HouseFix/assests/script/script.js
+++ HouseFix/assests/script/script.js
@@ -1,17 +1,1 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const toggler = document.querySelector(".navbar-toggler");
-//   const collapse = document.querySelector(".navbar-collapse");
-//   const closeBtn = document.querySelector(".close-btn");
-
-//   function closeMenu() {
-//       collapse.classList.remove("show");
-
-//   }
-
-//   toggler.addEventListener("click", function () {
-//       collapse.classList.toggle("show");
-//   });
-
-//   closeBtn.addEventListener("click", closeMenu);
-
-// });
+// 
